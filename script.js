25c5b0ceebbb578e96c4468a8ea52cf375a20b50
OofCoding <52016586+OofCoding@users.noreply.github.com>
2021-02-10 15:27:42 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+const show = (classname) => {
+    const elements = document.getElementsByClassName(classname);
+    for (let i = 0; i < elements.length; i++) {
+        elements[i].style.display = "block";
+    }
+}
+
+const showAll = () => {
+    show("anchor");
+    show("blue");
+    show("red");
+    show("eight");
+    
+    document.getElementById("main-container").style.alignItems = "center";
+    document.getElementById("btn").style.display = "none";
+}
+
+const setDisplay = (day) => {
+    if (day == 1) {
+        show("anchor");
+    }
+    else if (day == 2 || day == 4) {
+        show("blue");
+    }
+    else if (day == 3 || day == 5) {
+        show("eight");
+        show("red");
+    }
+}
+
+const currDate = new Date();
+const dayOfTheWeek = currDate.getDay();
+setDisplay(dayOfTheWeek);
